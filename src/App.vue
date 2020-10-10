d6c0d4772b7926f454767933d7cef607ada01a8c
Bruno Steinmann <bruno.steinmann@gmx.com>
2020-10-10 19:46:02 +0200

    added app-bar and footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <v-app>
-    <v-card width="400" class="mx-auto mt-5">
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text rounded>Home</v-btn>
+      <v-btn text rounded>Login</v-btn>
+    </v-app-bar>
+    <v-card width="400" class="mx-auto mt-15">
       <v-card-title class="pb-0">
         <h2 class="display-1">Welcome to the App</h2>
       </v-card-title>
@@ -25,6 +31,25 @@
         <v-btn color="info">Create Event</v-btn>
       </v-card-actions>
     </v-card>
+
+    <v-footer color="primary lighten-1" padless>
+      <v-layout justify-center wrap>
+<!--        <v-btn-->
+<!--            v-for="link in links"-->
+<!--            :key="link"-->
+<!--            color="white"-->
+<!--            text-->
+<!--            rounded-->
+<!--            class="my-2"-->
+<!--        >-->
+<!--          {{ link }}-->
+<!--        </v-btn>-->
+        <v-flex primary lighten-2 py-4 text-center white--text xs12>
+          {{ new Date().getFullYear() }} — <strong>Vuetify Dashboard</strong>
+        </v-flex>
+      </v-layout>
+    </v-footer>
+
   </v-app>
 </template>
 
