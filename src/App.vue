2a3a5760809d705b9d47d36f88c0d64259634992
Bruno Steinmann <bruno.steinmann@gmx.com>
2020-10-10 19:33:38 +0200

    added vuetify form to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,40 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-card width="400" class="mx-auto mt-5">
+      <v-card-title class="pb-0">
+        <h2 class="display-1">Welcome to the App</h2>
+      </v-card-title>
+      <v-card-text>
+        <h4>Rails Api with Vuetify</h4>
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-form>
+        <v-text-field label="Username" prepend-icon="mdi-account-circle" />
+        <v-text-field
+          :type="showPassword ? 'text' : 'password'"
+          label="Password"
+          prepend-icon="mdi-lock"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append="showPassword = !showPassword"
+        />
+      </v-form>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-btn color="info">Event-List</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn color="info">Create Event</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+export default {
+  name: "App",
+  data () {
+    return {
+      showPassword: false
+    };
+  }
+};
+</script>
