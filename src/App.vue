46940ce9ab56f00122e407424d93b0202b77d7d9
Bruno Steinmann <bruno.steinmann@gmx.com>
2020-10-10 19:54:33 +0200

    added links to footer

--- src/App.vue
+++ src/App.vue
@@ -34,31 +34,31 @@
 
     <v-footer color="primary lighten-1" padless>
       <v-layout justify-center wrap>
-<!--        <v-btn-->
-<!--            v-for="link in links"-->
-<!--            :key="link"-->
-<!--            color="white"-->
-<!--            text-->
-<!--            rounded-->
-<!--            class="my-2"-->
-<!--        >-->
-<!--          {{ link }}-->
-<!--        </v-btn>-->
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          text
+          rounded
+          class="my-2"
+        >
+          {{ link }}
+        </v-btn>
         <v-flex primary lighten-2 py-4 text-center white--text xs12>
           {{ new Date().getFullYear() }} — <strong>Vuetify Dashboard</strong>
         </v-flex>
       </v-layout>
     </v-footer>
-
   </v-app>
 </template>
 
 <script>
 export default {
   name: "App",
-  data () {
+  data() {
     return {
-      showPassword: false
+      showPassword: false,
+      links: ["Home", "Login"]
     };
   }
 };
