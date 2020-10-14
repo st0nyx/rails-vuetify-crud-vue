7bc4998abaebeaebe817ce80a1c0269c734c6b91
Bruno Steinmann <bruno.steinmann@gmx.com>
2020-10-14 10:43:23 +0200

    added lodash lib and automatic global component registration

--- src/main.js
+++ src/main.js
@@ -5,10 +5,28 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import i18n from "@/plugins/i18n";
 import FlagIcon from "vue-flag-icon";
+import upperFirst from "lodash/upperFirst";
+import camelCase from "lodash/camelCase";
 
 Vue.use(FlagIcon);
 Vue.component("EventShow");
 Vue.config.productionTip = false;
+
+const requireComponent = require.context(
+  "./components",
+  false,
+  /Base[A-Z]\w+\.(vue|js)$/
+);
+
+requireComponent.keys().forEach(fileName => {
+  const componentConfig = requireComponent(fileName);
+
+  const componentName = upperFirst(
+    camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, "$1"))
+  );
+
+  Vue.component(componentName, componentConfig.default || componentConfig);
+});
 
 new Vue({
   router,
